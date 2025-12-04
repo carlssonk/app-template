f79749c8c7006d47fc5b849c0ebf98a6bea3ca33
carlssonk <o.carlssonk@gmail.com>
2025-12-04 19:30:55 +0100

    Update index_document: main/index.html

--- terraform/environments/staging-and-production/main.tf
+++ terraform/environments/staging-and-production/main.tf
@@ -13,10 +13,13 @@ provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
 
+# Bucket name = "{subdomain}.{root_domain}"
 module "website" {
   source      = "github.com/carlssonk/terraform-modules//compositions/cloudflare-cdn-website?ref=main"
   root_domain = var.root_domain
   subdomain = var.subdomain
+
+  index_document = "main/index.html"
 
   tags = {
     Environment  = var.environment
