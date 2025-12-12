e4a4c5dfcef35d448e10bab2ca738d6dd5ce0220
carlssonk <o.carlssonk@gmail.com>
2025-12-12 20:57:34 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <div>
-      <h1>Latest Version :D</h1>
+      <h1>:D</h1>
       <p style={{ fontSize: '0.8em', opacity: 0.5, marginTop: '2rem' }}>
         Version: {__COMMIT_HASH__}
       </p>
