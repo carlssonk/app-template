0bf109daebaa8fcb12047113a7957aa9ec4221a0
carlssonk <o.carlssonk@gmail.com>
2025-12-12 19:10:04 +0100

    div

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import './App.css'
 function App() {
 
   return (
-    <>
+    <div>
       <h1>:D</h1>
       <p style={{ fontSize: '0.8em', opacity: 0.5, marginTop: '2rem' }}>
         Version: {__COMMIT_HASH__}
       </p>
-    </>
+    </div>
   )
 }
 
